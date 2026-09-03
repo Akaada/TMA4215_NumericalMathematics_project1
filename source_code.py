306beb707978b5e2eeadc0dff6743c9f313d4ad1
Andreas Kaada <andrkaa@stud.ntnu.no>
2026-09-03 11:18:33 +0200

    prøver å lage en klasse for å lettere kunne finne error senere

--- source_code.py
+++ source_code.py
@@ -70,7 +70,7 @@ def runge_function(x):
     Returns:
     ndarray[float]: The values of the Runge function at x.
     """
-    return 1 / (1 + 25 * x**2) # return the value of the Runge function
+    return 1 / (1 + x**2) # return the value of the Runge function
 
 def func1(x):
     """
@@ -133,3 +133,80 @@ def plot_interpolation(start,end,function, num_points=10, point_type= 'equidista
     plt.legend()
     plt.grid()
     plt.show()
+
+
+
+class LagrangeInterpolator:
+    """
+    A class to perform Lagrange interpolation for a given set of points.
+    """
+
+    def __init__(self,start,end, **kwargs):
+        """
+        Initialize the LagrangeInterpolator with x and y values.
+
+        Parameters:
+        **kwargs: Arbitrary keyword arguments. Must include 'x_values' and 'y_values'.
+        """
+        self.start = start
+        self.end = end
+        self.num_points = kwargs.get('num_points', 10)
+        self.func_name = kwargs.get('function', 'runge_function')
+        self.func = getattr(self, self.func_name)
+        self.point_function = getattr(self,f"{kwargs.get('point_type', 'equidistant')}_points")
+        self.x_values = self.point_function()
+        self.y_values = self.func(np.array(self.x_values))
+
+    # functions to generate the different types of points for interpolation
+    def equidistant_points(self):
+        """
+        Generate equidistant points between the start and end values. Stores the points in self.x_values.
+        """
+        step = (self.end - self.start) / (self.num_points - 1)
+        return [self.start + i * step for i in range(self.num_points)]
+
+    def chebyshev_points(self):
+        """
+        Generate Chebyshev points between the start and end values. Stores the points in self.x_values.
+        """
+        integers = np.arange(self.num_points)
+        cheb_untransformed = np.cos((integers + 0.5)*np.pi/self.num_points)
+        return (self.start + self.end)/2 +  cheb_untransformed*(self.end - self.start)/2
+
+
+    # functions to evaluate the different functions for interpolation
+    def runge_function(self, x):
+        """
+        Evaluate the Runge function at a given x.
+
+        Parameters:
+        x (ndarray[float]): The x-values at which to evaluate the Runge function.
+
+        Returns:
+        ndarray[float]: The values of the Runge function at x.
+        """
+        return 1 / (1 + x**2)
+
+    def func1(self, x):
+        """
+        Evaluate the function f(x) = cos(2πx) at a given x.
+
+        Parameters:
+        x (ndarray[float]): The x-values at which to evaluate the function.
+
+        Returns:
+        ndarray[float]: The values of the function at x.
+        """
+        return np.cos(2*np.pi*x)
+
+    def func2(self, x):
+        """
+        Evaluate the function f(x) = e^(3x) * sin(2x) at a given x.
+
+        Parameters:
+        x (ndarray[float]): The x-values at which to evaluate the function.
+
+        Returns:
+        ndarray[float]: The values of the function at x.
+        """
+        return np.exp(3*x)*np.sin(2*x)
